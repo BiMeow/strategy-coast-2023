842ecac9d44aa62d0015ba03a0e726a25ec440b0
BiMeow <trungnguyen9879@gmail.com>
2023-09-28 14:11:23 +0700

    fix(fe): change pic about page brand

--- src/components/elements/Loading.tsx
+++ src/components/elements/Loading.tsx
@@ -1,29 +1,45 @@
+import React from "react";
+import { useEffect, useState, useContext, useRef, memo, useMemo } from "react";
+import { useRouter } from "next/router";
 import { useStorage } from "@/components/context/StorageProvider";
 
-const Loading = () => {
+function Loading({ ...props }) {
+  const router = useRouter();
+
   const { isLoading } = useStorage();
 
   return (
     <>
-      <div
-        className={`
-        wrapLoading fixed inset-0 z-[9999] flex h-screen w-screen items-center justify-center overflow-hidden duration-700
-        ${isLoading ? "" : "scale-0 rounded-full opacity-0"}
-        `}
-      >
-        <div className="overlay absolute left-0 top-0 h-full w-full //bg-[#0007] bg-greenDark backdrop-blur-[40px]"></div>
-        <div className="relative w-[200px] rounded-full overflow-hidden bg-greenDark">
-          <div className="aspect-1"></div>
-          <img
-            src="/images/loader.png"
-            alt=""
-            className="move absolute w-[500px] h-full max-w-none top-0 left-0"
-          />
+      <div className={`Loading`}>
+        <div
+          className={`
+          fixed inset-0 z-[9999] flex h-screen w-screen items-center justify-center overflow-hidden duration-700
+          ${isLoading ? "" : "scale-0 rounded-full opacity-0"}
+          `}
+        >
+          <div className="overlay absolute left-0 top-0 h-full w-full //bg-[#0007] bg-greenDark backdrop-blur-[40px]"></div>
+          <div className="relative w-[200px] rounded-full overflow-hidden bg-greenDark">
+            <div className="aspect-1"></div>
+            <img
+              src="/images/loader.png"
+              alt=""
+              className="move absolute w-[500px] h-full max-w-none top-0 left-0"
+            />
+          </div>
         </div>
       </div>
-      <style jsx>{`
-        .move {
-          animation: marquee 5s linear infinite;
+
+      <style jsx global>{`
+        .Loading {
+          .move {
+            animation: marquee 5s linear infinite;
+          }
+
+          @media screen and (max-width: 767px) {
+            .move {
+              opacity: opacity 5s linear infinite;
+            }
+          }
         }
 
         @keyframes opacity {
@@ -46,6 +62,6 @@ const Loading = () => {
       `}</style>
     </>
   );
-};
+}
 
-export default Loading;
+export default memo(Loading);
